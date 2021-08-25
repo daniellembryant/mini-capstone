8ae76c933c7ed528869d9e86419a95085a3add1e
daniellembryant <danbry08@yahoo.com>
2021-08-25 11:58:40 -0500

    Adds product to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product = Product.new(name: "CD:Writings on the Wall", price: 9.99, image_url: "https://upload.wikimedia.org/wikipedia/en/f/f9/Destiny%27s_Child_%E2%80%93_The_Writing%27s_on_the_Wall.jpg", description: "Destiny's Child cd")
+product.save
