54a83f417db75364382165b423cd2736abf26dff
daniellembryant <danbry08@yahoo.com>
2021-09-11 20:31:51 -0500

    Changes create action calculations and show and index actions to change to make sure the user is loggeed in.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,26 +2,48 @@ class OrdersController < ApplicationController
   def create
     order = Order.new(
       user_id: current_user.id,
-      product_id: params[:product_id], 
+      product_id: params[:product_id],
       quantity: params[:quantity], 
-      
-      subtotal: params[:subtotal], 
-      tax: params[:tax],
-      total: params[:total]
+
     )
-    order.save
-    render json: order
+
+    order.subtotal = order.quantity * order.product.price
+    order.tax = order.subtotal * 0.09
+    order.total = order.subtotal + order.tax
+
+    if order.save
+      render json: order
+      
+    else
+      render json: order.errors.full_messages, status: :unprocessable_entity
+    end
+    
+      
+  
   end
 
   def show
-    orders = Order.find(params[:id])
-    render json: orders
-    
+    if current_user
+      order = Order.find(params[:id])
+      if order.user_id ==        current_user.id
+        render json: order
+      else
+        render json: {message: "That is not your order!"}, status: 401
+      end
+    else
+      render json: {message: "You must be logged in to view your order"}
+    end
+      
   end
 
   def index
-    orders = Order.all
-    render json: orders
+    if current_user
+      orders = Order.all
+      render json: orders
+    else
+      render json: {message: "You must be logged in to see your orders."}
+
+    end
     
   end
 end
